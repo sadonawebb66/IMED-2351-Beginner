5704abb3a1b4c4b031cb33815a0a5d55c0c17e64
sadonawebb66 <sadonawebb.66@gmail.com>
2023-04-06 18:21:30 -0500

    emoji made

---
+++ app.js
@@ -0,0 +1,16 @@
+const shadesFace=document.querySelector('.closed');
+const pukeFace=document.querySelector('.open');
+//add event listener
+shadesFace.addEventListener('click', ()=>{
+    if(pukeFace.classList.contains('open')){
+        pukeFace.classList.add('active');
+        shadesFace.classList.remove('active');
+    }
+});
+pukeFace.addEventListener('click',()=>{
+    if(shadesFace.classList.contains('closed')){
+        shadesFace.classList.add('active');
+        pukeFace.classList.remove('active');
+
+    }
+});
